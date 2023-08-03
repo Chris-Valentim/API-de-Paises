f63d60382ac4dfcdedc765c8e4bf0356b049a271
Christian Valentim <christianvalentim99@gmail.com>
2023-08-03 01:04:01 -0300

    feat: criação das paginas

---
+++ src/page/Home/index.tsx
@@ -0,0 +1,18 @@
+import { Card } from "../../components/Card"
+import { useFlags } from "../../hooks/useFlags"
+
+
+const Home = () => {
+
+  const { allFlags }  = useFlags()
+
+  return (
+    <div className='flex flex-wrap'>
+      {allFlags.map((item) => (
+        <Card capital={item.capital} flags={item.flags.svg} name={item.name.common} population={item.population} region={item.region} />
+      ))}
+    </div>
+  )
+}
+
+export { Home }
